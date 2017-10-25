c6e596eea931870771c72b2d19af75b0d6558bc6
Eric <daoudattoyan.eric@gmail.com>
2017-10-25 13:43:31 +0200

    /update_playlist is now POST

--- flask-server/application/app.py
+++ flask-server/application/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify, redirect, request, url_for
+from flask import Flask, jsonify, redirect, request
 from spotify_api import SpotifyApi
 from kshe_scraper import KSHEScraper
 import logging
-import uwsgi
 
 app = Flask(__name__)
 api = SpotifyApi()
@@ -11,7 +10,7 @@ scraper = KSHEScraper('http://player.listenlive.co/20101/en/songhistory')
 
 # def update_playlist(signum):
 #     logging.info('signum {0} received, will update playlist'.format(signum))
-    
+
 #     return redirect(url_for('update_playlist'))
 
 
@@ -21,30 +20,30 @@ scraper = KSHEScraper('http://player.listenlive.co/20101/en/songhistory')
 
 @app.route('/')
 def index():
-    return jsonify( _api_version='1.0')
+    return jsonify(_api_version='1.0')
 
 
-@app.route('/scrap')
+@app.route('/scrap', methods=['GET'])
 def scrap():
-    song_history=scraper.get_song_history()
+    song_history = scraper.get_song_history()
 
     return jsonify(song_history)
 
 
-@app.route('/update_playlist', methods=['GET','POST'])
+@app.route('/update_playlist', methods=['POST'])
 def update_playlist():
     song_history = scraper.get_song_history()
-    spotify_songs = [api.search_track(s['title'], s['artist']) \
+    spotify_songs = [api.search_track(s['title'], s['artist'])
                         for s in song_history]
 
     spotify_songs = [s for s in spotify_songs if len(s) > 0]
-    
+
     try:
         uris_in_playlist = api.get_track_uris_from_playlist(api.playlist_uri)
     except:
         uris_in_playlist = set()
 
-    tracks_to_be_added = [s['spotify_uri'] for s in spotify_songs \
+    tracks_to_be_added = [s['spotify_uri'] for s in spotify_songs
                             if s['spotify_uri'] not in uris_in_playlist]
 
     logging.info('will add {0} tracks'.format(len(tracks_to_be_added)))
@@ -54,16 +53,15 @@ def update_playlist():
     return jsonify(**response)
 
 
-@app.route('/auth')
+@app.route('/auth', methods=['GET'])
 def auth():
     return redirect(api._authorization_code_flow_authentication())
 
 
 @app.route('/callback')
 def callback():
-    response = api._client_credentials_authentication(
-            request.args['code'],
-            api.redirect_uri)
+    response = api._client_credentials_authentication(request.args['code'],
+                                                      api.redirect_uri)
 
     api._access_token = response["access_token"]
     token_type = response["token_type"]
@@ -71,8 +69,6 @@ def callback():
 
     logging.info('authenticated')
 
-    return jsonify(
-            authenticated=True,
-            token_type=token_type,
-            token_expires_in=api._token_expires_in
-        )
+    return jsonify(authenticated=True,
+                   token_type=token_type,
+                   token_expires_in=api._token_expires_in)
